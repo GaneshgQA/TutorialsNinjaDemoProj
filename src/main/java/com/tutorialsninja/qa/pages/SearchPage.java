98021af1277818d2a3b00a976c5200bc20729f6b
Freq <Freq@DESKTOP-AHFSB1P>
2026-06-21 13:10:49 +0530

    test

--- src/main/java/com/tutorialsninja/qa/pages/SearchPage.java
+++ src/main/java/com/tutorialsninja/qa/pages/SearchPage.java
@@ -160,7 +160,9 @@ public class SearchPage {
 	
 	public boolean isProductHavingDescriptionTextDisplayedInSearchResult() {
 		
-		return productHavingSearchTextInDescription.isDisplayed();
+		//return productHavingSearchTextInDescription.isDisplayed();
+		
+	 return elementUtils.isElementDisplayed(productHavingSearchTextInDescription);
 		
 	}
 
