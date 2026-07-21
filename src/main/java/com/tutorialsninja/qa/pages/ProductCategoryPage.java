bc00ade39f5e043c4bd7cddb6259ff3fc879516e
Ganesh <gmhealthcare7@gmail.com>
2026-07-21 19:37:08 +0530

    Product category test modified

--- src/main/java/com/tutorialsninja/qa/pages/ProductCategoryPage.java
+++ src/main/java/com/tutorialsninja/qa/pages/ProductCategoryPage.java
@@ -33,22 +33,36 @@ public class ProductCategoryPage extends ProductDisplayPage {
 	@FindBy(xpath = "//div[contains(@class,'product-layout product-list')]//h4//a")
 	private List<WebElement> productListInProductCatagoryPage;
 
-	
-	public List<String> getNameOfTheProductInProductCatagoryPage() {
+	public List<String> getNameOfTheProductInProductCatagoryPage() throws InterruptedException {
 
 		wait.until(ExpectedConditions.visibilityOfAllElements(productListInProductCatagoryPage));
 
 		List<String> productNames = new ArrayList<>();
+		WebElement targetElement = null;
 
 		for (WebElement productList : productListInProductCatagoryPage) {
 
 			String text = elementUtils.getTextOfElement(productList);
 			System.out.println(text);
 			productNames.add(text);
+
+			if (productList.getText().trim().equalsIgnoreCase("iPod Classic")) {
+
+				wait.until(ExpectedConditions.elementToBeClickable(productList));
+				Thread.sleep(5000);
+				targetElement = productList;
+
+				if (targetElement != null) {
+					wait.until(ExpectedConditions.elementToBeClickable(targetElement));
+					Thread.sleep(5000); // Optional, consider removing if wait is enough
+					targetElement.click();
+					break;
+				}
+				// productList.click();
+			}
+
 		}
-
+		
 		return productNames;
-
 	}
-
 }
