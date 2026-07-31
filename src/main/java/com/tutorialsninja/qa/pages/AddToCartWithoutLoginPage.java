5e59d9e19fa7d68987ebee8d8b1321d4f6017dc9
Ganesh <gmhealthcare7@gmail.com>
2026-07-31 18:28:46 +0530

    created page class and test class for addingproducttocartWithoutlogin

---
+++ src/main/java/com/tutorialsninja/qa/pages/AddToCartWithoutLoginPage.java
@@ -0,0 +1,101 @@
+package com.tutorialsninja.qa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.tutorialsnija.qa.utils.ElementUtils;
+
+public class AddToCartWithoutLoginPage {
+
+	WebDriver driver;
+	ElementUtils elementUtils;
+	
+	public AddToCartWithoutLoginPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		elementUtils = new ElementUtils(driver);
+	}
+	
+	// Web Elements for MacBook product
+	@FindBy(xpath = "//a[text()='MacBook']")
+	private WebElement macbookProduct;
+	
+	// Web Element for Add to Cart button
+	@FindBy(xpath = "//button[contains(@onclick, 'cart.add')]")
+	private WebElement addToCartButton;
+	
+	// Web Element for success message
+	@FindBy(xpath = "//div[contains(@class, 'alert alert-success alert-dismissible')]")
+	private WebElement successMessage;
+	
+	// Web Element for shopping cart link
+	@FindBy(xpath = "//span[text()='Shopping Cart']")
+	private WebElement shoppingCartLink;
+	
+	// Action Methods
+	
+	/**
+	 * Check if MacBook product is available and then click on Add to Cart button
+	 */
+	public void clickOnMacbookProduct() {
+		if (isMacbookProductDisplayed()) {
+			elementUtils.clickOnElements(addToCartButton);
+		}
+	}
+	
+	/**
+	 * Click on Add to Cart button to add MacBook to cart
+	 */
+	public void clickOnAddToCartButton() {
+		elementUtils.clickOnElements(addToCartButton);
+	}
+	
+	/**
+	 * Get the success message text after adding product to cart
+	 * @return Success message text
+	 */
+	public String getTextOfSuccessMessage() {
+		return elementUtils.getTextOfElement(successMessage);
+	}
+	
+	/**
+	 * Click on Shopping Cart link to navigate to shopping cart page
+	 */
+	public void clickOnShoppingCartLink() {
+		elementUtils.clickOnElements(shoppingCartLink);
+	}
+	
+	/**
+	 * Verify if MacBook product is displayed
+	 * @return true if MacBook is displayed, false otherwise
+	 */
+	public boolean isMacbookProductDisplayed() {
+		return elementUtils.isElementDisplayed(macbookProduct);
+	}
+	
+	/**
+	 * Verify if Add to Cart button is displayed
+	 * @return true if Add to Cart button is displayed, false otherwise
+	 */
+	public boolean isAddToCartButtonDisplayed() {
+		return elementUtils.isElementDisplayed(addToCartButton);
+	}
+	
+	/**
+	 * Verify if success message is displayed
+	 * @return true if success message is displayed, false otherwise
+	 */
+	public boolean isSuccessMessageDisplayed() {
+		return elementUtils.isElementDisplayed(successMessage);
+	}
+	
+	/**
+	 * Verify if Shopping Cart link is displayed
+	 * @return true if Shopping Cart link is displayed, false otherwise
+	 */
+	public boolean isShoppingCartLinkDisplayed() {
+		return elementUtils.isElementDisplayed(shoppingCartLink);
+	}
+}
---
+++ src/test/java/com/tutorialsninjademo/qa/testcases/AddToCartWithoutLoginTest.java
@@ -0,0 +1,105 @@
+package com.tutorialsninjademo.qa.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.tutorialsninja.qa.base.Base;
+import com.tutorialsninja.qa.pages.AddToCartWithoutLoginPage;
+import com.tutorialsninja.qa.pages.HomePage;
+
+public class AddToCartWithoutLoginTest extends Base {
+
+	WebDriver driver;
+	AddToCartWithoutLoginPage addToCartWithoutLoginPage;
+	HomePage homePage;
+
+	public AddToCartWithoutLoginTest() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setUp() {
+		driver = initializeBrowserAndOpenApplicationURL(prop.getProperty("browser"));
+		homePage = new HomePage(driver);
+		addToCartWithoutLoginPage = new AddToCartWithoutLoginPage(driver);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+
+	@Test(priority = 1)
+	public void verifyAddingMacbookToCartWithoutLogin() {
+		Assert.assertTrue(addToCartWithoutLoginPage.isMacbookProductDisplayed(),
+				"MacBook product is not displayed on the home page");
+		Assert.assertTrue(addToCartWithoutLoginPage.isAddToCartButtonDisplayed(),
+				"Add to Cart button is not displayed");
+		addToCartWithoutLoginPage.clickOnAddToCartButton();
+
+		// Wait a moment for the success message to appear
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Assert.assertTrue(addToCartWithoutLoginPage.isSuccessMessageDisplayed(),
+				"Success message is not displayed after adding product to cart");
+
+		String successMessage = addToCartWithoutLoginPage.getTextOfSuccessMessage();
+		Assert.assertNotNull(successMessage, "Success message is null");
+		Assert.assertTrue(successMessage.length() > 0, "Success message is empty");
+		System.out.println("Success Message: " + successMessage);
+		
+		Assert.assertTrue(addToCartWithoutLoginPage.isShoppingCartLinkDisplayed(),
+				"Shopping Cart link is not displayed after adding product to cart");
+	}
+	
+	@Test(priority = 2)
+	public void verifyCompleteAddToCartFlowWithoutLogin() {
+		// Click on Add to Cart button
+		addToCartWithoutLoginPage.clickOnAddToCartButton();
+
+		// Wait for success message
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Verify success message is displayed
+		Assert.assertTrue(addToCartWithoutLoginPage.isSuccessMessageDisplayed(),
+				"Success message is not displayed after adding product to cart");
+
+		// Get and verify success message
+		String successMessage = addToCartWithoutLoginPage.getTextOfSuccessMessage();
+		Assert.assertNotNull(successMessage, "Success message is null");
+		System.out.println("Success Message: " + successMessage);
+
+		// Click on Shopping Cart link to navigate to cart
+		addToCartWithoutLoginPage.clickOnShoppingCartLink();
+
+		// Wait for page to load
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// Verify navigation to shopping cart page
+		String pageTitle = getPageTitle(driver);
+		System.out.println("Page Title: " + pageTitle);
+		Assert.assertTrue(pageTitle.contains("Shopping Cart") || pageTitle.contains("cart"),
+				"Failed to navigate to shopping cart page");
+	}
+
+	/**
+	 * Test to verify MacBook product can be searched and added to cart
+	 */
+
+}
